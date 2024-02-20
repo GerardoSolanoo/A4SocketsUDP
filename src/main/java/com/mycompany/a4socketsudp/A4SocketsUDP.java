bde59ac2085cf6b26d7e661644df4b43310babe2
GerardoSolanoo <gerardosolano.contact@gmail.com>
2024-02-20 11:24:06 -0600

    base code

---
+++ src/main/java/com/mycompany/a4socketsudp/A4SocketsUDP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.a4socketsudp;
+
+/**
+ *
+ * @author Gerar
+ */
+public class A4SocketsUDP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
